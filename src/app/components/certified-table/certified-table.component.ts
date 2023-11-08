0d7acda740b31a945388395b17b3884ea2648e24
Andres Negrette <andresdngtt@gmail.com>
2023-11-07 23:02:11 -0300

    Integracion con imagenes almacenadas en Amazon S3

--- src/app/components/certified-table/certified-table.component.ts
+++ src/app/components/certified-table/certified-table.component.ts
@@ -25,53 +25,77 @@ export class CertifiedTableComponent {
       year: 'numeric',
     });
 
-    const imageUrl = 'assets/headerPdf.jpg';
+    const imageHeaderUrl = 'https://notazer0.s3.amazonaws.com/headerPdf.jpg';
+    const imageSignatureUrl =
+      'https://notazer0.s3.amazonaws.com/firma-ifts11.png';
 
     // Cargar la imagen utilizando HttpClient
     this.http
-      .get(imageUrl, { responseType: 'blob' })
-      .subscribe((blob: Blob) => {
-        const reader = new FileReader();
+      .get(imageHeaderUrl, { responseType: 'blob' })
+      .subscribe((headerBlob: Blob) => {
+        this.http
+          .get(imageSignatureUrl, { responseType: 'blob' })
+          .subscribe((signatureBlob: Blob) => {
+            const readerHeader = new FileReader();
+            const readerSignature = new FileReader();
 
-        reader.onload = () => {
-          const imageBase64 = reader.result as string;
+            readerHeader.onload = () => {
+              readerSignature.onload = () => {
+                const imageHeaderBase64 = readerHeader.result as string;
+                const imageSignatureBase64 = readerSignature.result as string;
 
-          const documentDefinition = {
-            content: [
-              {
-                image: imageBase64,
-                width: 480,
-                style: [{ alignment: 'center' }],
-              }, // Utiliza la representación base64 de la imagen
-              {
-                text: certificado.tipoConstancia,
-                fontSize: 14,
-                style: [{ alignment: 'center', bold: true, lineHeight: 2 }],
-              },
-              {
-                text:
-                  'Por la presente se deja constancia que ' +
-                  certificado.nombre +
-                  ' DNI/LC/LE N° ' +
-                  certificado.dni +
-                  ' se encuentra inscripto/a en la carrera de ' +
-                  certificado.carrera +
-                  ', que se dicta en este establecimiento de lunes a viernes de 18:30 a 22:30 horas.-----',
-                fontSize: 14,
-              },
-              {
-                text: `Se extiende la presente a los ${dia} días del mes de ${mesAnio}, a pedido del interesado/a, para ser presentada ante: ${certificado.aSerPresentadoAnte}`,
-                fontSize: 14,
-              },
-            ],
-          };
+                const documentDefinition = {
+                  content: [
+                    {
+                      text: `Código de validación: ${certificado.certifiedId}`,
+                      fontSize: 8,
+                      style: [{ alignment: 'center' }],
+                    },
+                    {
+                      image: imageHeaderBase64,
+                      width: 480,
+                      style: [{ alignment: 'center' }],
+                    },
+                    {
+                      text: certificado.tipoConstancia,
+                      fontSize: 14,
+                      style: [
+                        { alignment: 'center', bold: true, lineHeight: 2 },
+                      ],
+                    },
+                    {
+                      text:
+                        'Por la presente se deja constancia que ' +
+                        certificado.nombre +
+                        ' DNI/LC/LE N° ' +
+                        certificado.dni +
+                        ' se encuentra inscripto/a en la carrera de ' +
+                        certificado.carrera +
+                        ', que se dicta en este establecimiento de lunes a viernes de 18:30 a 22:30 horas.-----',
+                      fontSize: 14,
+                    },
+                    {
+                      text: `Se extiende la presente a los ${dia} días del mes de ${mesAnio}, a pedido del interesado/a, para ser presentada ante: ${certificado.aSerPresentadoAnte}`,
+                      fontSize: 14,
+                    },
+                    {
+                      image: imageSignatureBase64,
+                      width: 360,
+                      style: [{ alignment: 'center' }],
+                    },
+                  ],
+                };
 
-          // Genera el PDF
-          const pdfDocGenerator = pdfMake.createPdf(documentDefinition);
-          pdfDocGenerator.download('constancia.pdf');
-        };
+                // Genera el PDF
+                const pdfDocGenerator = pdfMake.createPdf(documentDefinition);
+                pdfDocGenerator.download('constancia.pdf');
+              };
 
-        reader.readAsDataURL(blob);
+              readerSignature.readAsDataURL(signatureBlob);
+            };
+
+            readerHeader.readAsDataURL(headerBlob);
+          });
       });
   }
 }
