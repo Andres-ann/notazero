20a292aaed71fe2ccb93f1e0e9e01b93bb2bb185
Andres <andresdngtt@gmail.com>
2023-11-08 22:43:31 -0300

    Se implementan las plantillas para el body de cada certificado

--- src/app/components/certified-table/certified-table.component.ts
+++ src/app/components/certified-table/certified-table.component.ts
@@ -20,14 +20,35 @@ export class CertifiedTableComponent {
   generatePDF(certificado: Certified) {
     const fecha = new Date(certificado.fecha);
     const dia = fecha.getDate(); // Extraer el día de la fecha
-    const mesAnio = fecha.toLocaleString('es', {
+    const mesAnio = fecha.toLocaleString('es-AR', {
       month: 'long',
       year: 'numeric',
     });
 
+    console.log(fecha);
+
+    // Define y almacena las imagenes en viariables
     const imageHeaderUrl = 'https://notazer0.s3.amazonaws.com/headerPdf.jpg';
-    const imageSignatureUrl =
-      'https://notazer0.s3.amazonaws.com/firma-ifts11.png';
+    let imageSignatureUrl =
+      certificado.estado === 'pendiente'
+        ? 'https://notazer0.s3.amazonaws.com/pending-review.png'
+        : 'https://notazer0.s3.amazonaws.com/firma-ifts11.png';
+
+    // Templates de texto
+    let template: String;
+    let isInscription = '';
+
+    if (certificado.tipoConstancia === 'CONSTANCIA DE ALUMNO REGULAR') {
+      template = `Por la presente se deja constancia que ${certificado.nombre} DNI/LC/LE N° ${certificado.dni} es alumno/a regular de la carrera ${certificado.carrera}, que se dicta en este establecimiento de lunes a viernes de 18.30 a 22.30 horas.------`;
+    } else if (certificado.tipoConstancia === 'CONSTANCIA DE INSCRIPCIÓN') {
+      template = `Por la presente se deja constancia que ${certificado.nombre} DNI/LC/LE N° ${certificado.dni} se encuentra inscripta/o en la carrera ${certificado.carrera}, que se dicta en este establecimiento de lunes a viernes de 18.30 a 22.30 horas.------`;
+
+      isInscription = `Se deja constancia que ${certificado.nombre} DNI: ${certificado.dni} es alumno de primer
+      año de nuestro establecimiento, pero para obtener la regularidad es necesario aprobar al menos una materia.-
+      `;
+    } else if (certificado.tipoConstancia === 'CONSTANCIA DE EXAMEN') {
+      template = `Por la presente se deja constancia que ${certificado.nombre} DNI/LC/LE N° ${certificado.dni} ha rendido examen ${certificado.tipoExamen} de la asignatura ${certificado.asignatura}.------`;
+    }
 
     // Cargar la imagen utilizando HttpClient
     this.http
@@ -47,11 +68,6 @@ export class CertifiedTableComponent {
                 const documentDefinition = {
                   content: [
                     {
-                      text: `Código de validación: ${certificado.certifiedId}`,
-                      fontSize: 8,
-                      style: [{ alignment: 'center' }],
-                    },
-                    {
                       image: imageHeaderBase64,
                       width: 480,
                       style: [{ alignment: 'center' }],
@@ -64,24 +80,31 @@ export class CertifiedTableComponent {
                       ],
                     },
                     {
-                      text:
-                        'Por la presente se deja constancia que ' +
-                        certificado.nombre +
-                        ' DNI/LC/LE N° ' +
-                        certificado.dni +
-                        ' se encuentra inscripto/a en la carrera de ' +
-                        certificado.carrera +
-                        ', que se dicta en este establecimiento de lunes a viernes de 18:30 a 22:30 horas.-----',
+                      text: `${template}`,
                       fontSize: 14,
                     },
+
                     {
                       text: `Se extiende la presente a los ${dia} días del mes de ${mesAnio}, a pedido del interesado/a, para ser presentada ante: ${certificado.aSerPresentadoAnte}`,
                       fontSize: 14,
+                      margin: [0, 10, 0, 0],
                     },
                     {
                       image: imageSignatureBase64,
                       width: 360,
                       style: [{ alignment: 'center' }],
+                    },
+                    {
+                      text: `Código único de identificación: ${certificado.certifiedId}`,
+                      fontSize: 8,
+                      style: [{ alignment: 'center' }],
+                    },
+                    {
+                      text: `${isInscription}`,
+                      fontSize: 14,
+                      italics: true,
+                      bold: true,
+                      margin: [0, 15, 0, 0],
                     },
                   ],
                 };
